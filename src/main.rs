f7ffd6b3965ce0c13abfcc87db5f37749c8d59de
Celeste_Tollec <celeste73310@gmail.com>
2026-02-05 19:25:42 +0100

    Mise en place d'un compute shader basique

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ struct App {
     gl: glow::Context,
     compute_program: glow::Program,
     output_tex: glow::Texture,
+    frame:i32, // dummy value
+    display_program: glow::Program, 
+    vao: glow::VertexArray,
 }
 
 fn main() {
@@ -81,9 +84,18 @@ fn init() -> App {
         tex
     };
 
-    let compute_src = include_str!("compute.glsl");
+    let compute_src = include_str!("shader/compute.glsl");
     let compute_program = create_compute_program(&gl, compute_src);
 
+    let vs_src = include_str!("shader/display.vert"); 
+    let fs_src = include_str!("shader/display.frag"); 
+    let display_program = create_program(&gl, vs_src, fs_src); 
+    
+    // VAO obligatoire en Core Profile 
+    
+    let vao = unsafe { gl.create_vertex_array().unwrap() }; unsafe { gl.bind_vertex_array(Some(vao)); }
+
+    let frame = 0;
 
     App {
         sdl,
@@ -93,6 +105,9 @@ fn init() -> App {
         gl,
         compute_program,
         output_tex,
+        frame,
+        display_program, 
+        vao,
     }
 }
 
@@ -119,9 +134,43 @@ fn create_compute_program(gl: &glow::Context, src: &str) -> glow::Program {
     }
 }
 
+fn create_program(gl: &glow::Context, vs_src: &str, fs_src: &str) -> glow::Program {
+    unsafe {
+        let vs = gl.create_shader(glow::VERTEX_SHADER).unwrap();
+        gl.shader_source(vs, vs_src);
+        gl.compile_shader(vs);
+        if !gl.get_shader_compile_status(vs) {
+            panic!("VS error: {}", gl.get_shader_info_log(vs));
+        }
+
+        let fs = gl.create_shader(glow::FRAGMENT_SHADER).unwrap();
+        gl.shader_source(fs, fs_src);
+        gl.compile_shader(fs);
+        if !gl.get_shader_compile_status(fs) {
+            panic!("FS error: {}", gl.get_shader_info_log(fs));
+        }
+
+        let program = gl.create_program().unwrap();
+        gl.attach_shader(program, vs);
+        gl.attach_shader(program, fs);
+        gl.link_program(program);
+
+        if !gl.get_program_link_status(program) {
+            panic!("Link error: {}", gl.get_program_info_log(program));
+        }
+
+        gl.delete_shader(vs);
+        gl.delete_shader(fs);
+
+        program
+    }
+}
+
+
 fn update(app: &mut App) {
     handle_event(app);
     render(app);
+    app.frame += 1;
 }
 
 fn handle_event(app: &mut App) {
@@ -133,15 +182,35 @@ fn handle_event(app: &mut App) {
 }
 
 fn render(app: &mut App) {
-    let gl = &app.gl;
     unsafe {
+        let gl = &app.gl;
+
         gl.use_program(Some(app.compute_program));
+
+        // test de passage de donnée
+        let frame_loc = gl.get_uniform_location(app.compute_program, "frame"); 
+        if let Some(loc) = frame_loc { 
+            gl.uniform_1_i32(Some(&loc), app.frame); 
+        }
 
         let gx = (WIDTH as u32 + 7) / 8;
         let gy = (HEIGHT as u32 + 7) / 8;
         gl.dispatch_compute(gx, gy, 1);
 
         gl.memory_barrier(glow::SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+        gl.use_program(Some(app.display_program)); 
+        gl.bind_vertex_array(Some(app.vao)); 
+        
+        gl.active_texture(glow::TEXTURE0); 
+        gl.bind_texture(glow::TEXTURE_2D, Some(app.output_tex)); 
+        
+        let loc = gl.get_uniform_location(app.display_program, "tex"); 
+        if let Some(loc) = loc { 
+            gl.uniform_1_i32(Some(&loc), 0); 
+        } 
+        
+        gl.draw_arrays(glow::TRIANGLES, 0, 3);
     }
     app.window.swap_window();
 }
